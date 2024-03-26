22af114b77b1759162831187afa637e53f71172d
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-26 23:06:15 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inqurier from "inquirer";
+
+// Asking Questions from users through Inqurier
+
+const answer = await inqurier.prompt([
+    {message: "Enter First Number", type: "number" , name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name : "secondNumber"},
+    {
+        message: "Select one operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition","Subtraction","Multiplication","Division"],
+    },
+]);
+
+console.log(answer);
+
+//coditional statements IF-ELSE
+if (answer.operator === "Addition"){
+   console.log(answer.firstNumber + answer.secondNumber)
+} 
+else if (answer.operator === "Subtraction"){
+   console.log(answer.firstNumber - answer.secondNumber)
+}
+else if (answer.operator === "Multiplication"){
+  console.log(answer.firstNumber * answer.secondNumber)
+}
+else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else{
+   console.log("Invaild Input")
+}
